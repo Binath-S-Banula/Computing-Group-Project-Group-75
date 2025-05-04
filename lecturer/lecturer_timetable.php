d95f8669ef0163068465a4ae8bc5319766161471
GunathilakaPADIV <145748400+GunathilakaPADIV@users.noreply.github.com>
2025-05-04 14:38:13 +0530

    lecturer timetable

---
+++ lecturer/lecturer_timetable.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php'; ?>
+
+
+
+                <?php include 'sections/timetable.php'; ?>
+                <?php include 'sections/proposal_form.php'; ?>
+                <?php include 'sections/submitted_proposals.php'; ?>
+
+
+            
+<?php include 'includes/footer.php'; ?>
