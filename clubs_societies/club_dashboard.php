dc6c2cae198afafed1032ad52a7faf69b3aa487f
BanulaRPBS <bsbanula@gmail.com>
2025-05-04 12:50:33 +0530

    developments

---
+++ clubs_societies/club_dashboard.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+
+
+
+    <div class="dashboard-title">
+        <h2>Welcome back, <?= htmlspecialchars($club_name) ?> ! Admin</h2>
+        <p>Club Admin Dashboard – Manage, Organize, Lead </p>
+    </div>
+
+
+            
+<?php include 'includes/footer.php'; ?>
